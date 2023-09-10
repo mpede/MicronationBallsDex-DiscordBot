c44b780de0cab73312e081f25d5257e4125c1047
BadPythonCoder <nuh@uh.uh>
2023-09-10 08:03:12 +0200

    A test and a little bit of refactoring

--- ballsdex/packages/battles/cog.py
+++ ballsdex/packages/battles/cog.py
@@ -1,14 +1,21 @@
 from discord import app_commands
 from discord.ext import commands
 from ballsdex.packages.battles.Battle import Battle
+from ballsdex.packages.players.countryballs_paginator import CountryballsViewer
 from ballsdex.core.bot import BallsDexBot
-from ballsdex.core.models import Player
+from ballsdex.core.models import Player, BallInstance
 from tortoise.exceptions import DoesNotExist
 import discord, random
 
 #class DeckSelectionView(discord.ui.View):
 #	def __init__(self
 
+async def BallSelectSingular(CountryballsViewer):
+	async def ball_selected(self, interaction: discord.Interaction, ball_instance: BallInstance):
+		await interaction.response.send("DUCK!")
+
+
+'''
 class DeckSelectionView(discord.ui.View):
 	def __init__(self, usera, userb, ballsA, ballsB, timeout=180):
 		super().__init__(timeout=timeout)
@@ -16,7 +23,8 @@ class DeckSelectionView(discord.ui.View):
 		self.userb = userb
 		self.ballsA = ballsA
 		self.ballsB = ballsB
-		self.somebodypressed = False
+		self.randomA = None
+		self.randomB = None
 
 	@discord.ui.button(label="Random", style=discord.ButtonStyle.gray)
 	async def randomButton(self, interaction: discord.Interaction, button):
@@ -27,26 +35,44 @@ class DeckSelectionView(discord.ui.View):
 	async def chooseButton(self, interaction: discord.Interaction, button):
 		await interaction.response.send_message(f"Has somebody chosen? {self.somebodypressed}")
 		self.somebodypressed = True
+''' # im just gonna do this later
+
+class DeckSelect(discord.ui.View):
+	def __init__(self, balls, users, timeout=180):
+		super().__init__(timeout=timeout)
+		self.balls = balls
+		self.users = users
+
+		self.deckA = []
+		self.deckB = []
+
+		self.optionsA = [discord.SelectOption()
+
 
 class BattleAcceptView(discord.ui.View):
-	def __init__(self, ballsA, ballsB, challenger, target, timeout=180):
+	def __init__(self, balls, users, timeout=180):
 		super().__init__(timeout=timeout)
-		self.target = target
-		self.challenger = challenger
-		self.ballsA = ballsA
-		self.ballsB = ballsB
+		#self.target = target
+		#self.challenger = challenger
+		#self.ballsA = ballsA
+		#self.ballsB = ballsB
+		self.balls = balls
+		self.users = users
+
 
 	@discord.ui.button(label="Accept", style=discord.ButtonStyle.green)
 	async def accept(self, interaction: discord.Interaction, button):
-		if not interaction.user == self.target:
+		if not interaction.user == self.users[1]:
 			await interaction.response.send_message(content=f"<@{interaction.user.id}> This message was not meant for you!", ephemeral=True)
 			return
 
-		await interaction.response.edit_message(content=f"> @{self.challenger.id} @{self.target.id} Please choose your method of deck selection", view=DeckSelectionView(self.challenger, self.target, self.ballsA, self.ballsB))
+		#await interaction.response.edit_message(content=f"<@{self.challenger.id}> <@{self.target.id}> Please choose your method of deck selection", view=DeckSelectionView(self.users, self.balls))
+		paginator = BallSelectSingular(interaction, self.balls[0])
+		paginator.start()
 
 	@discord.ui.button(label="Decline", style=discord.ButtonStyle.red)
 	async def decline(self, interaction: discord.Interaction, button):
-		if not interaction.user == self.target:
+		if not interaction.user == self.users[1]:
 			await interaction.response.send_message(content=f"<@{interaction.user.id}> This message was not meant for you!", ephemeral=True)
 			return
 
@@ -72,4 +98,4 @@ class Battles(commands.GroupCog):
 		except DoesNotExist:
 			await interaction.response.send_message("You or your opponent do not have enough balls to partake in battle!", ephemeral=True)
 			return
-		await interaction.response.send_message(f"<@{opponent.id}>, <@{interaction.user.id}> wants to battle you!\nDo you accept?", view=BattleAcceptView(ballsA, ballsB, interaction.user, opponent))
+		await interaction.response.send_message(f"<@{opponent.id}>, <@{interaction.user.id}> wants to battle you!\nDo you accept?", view=BattleAcceptView((ballsA, ballsB), (interaction.user, opponent)))
