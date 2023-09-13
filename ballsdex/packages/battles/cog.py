f5da118dac333ad8f39104ca7b5740189939c986
BadPythonCoder <xsare435@gmail.com>
2023-09-13 16:12:20 +0200

    Hopefully work???

--- ballsdex/packages/battles/cog.py
+++ ballsdex/packages/battles/cog.py
@@ -63,7 +63,7 @@ class BattleAcceptView(discord.ui.View):
 			return
 
 		#await interaction.response.edit_message(content=f"<@{self.challenger.id}> <@{self.target.id}> Please choose your method of deck selection", view=DeckSelectionView(self.users, self.balls))
-		paginator = BallSelectSingular(interaction=interaction, balls=self.balls[0])
+		paginator = BallSelectSingular(interaction, self.balls[0])
 		#paginator.start()
 
 	@discord.ui.button(label="Decline", style=discord.ButtonStyle.red)
